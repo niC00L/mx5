85fa992335510b974395262fd68cb71bf1c0b269
niC00L <nika.blanarova@gmail.com>
2019-03-29 23:18:15 +0100

    New gitignore

--- docs/js/script.js
+++
@@ -1,7 +0,0 @@
-$('#mainNav').on('show.bs.collapse', function () {
-    $('#mainNav').addClass('open');
-});
-
-$('#mainNav').on('hide.bs.collapse', function () {
-    $('#mainNav').removeClass('open');
-});
